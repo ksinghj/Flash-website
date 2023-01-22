1177d62b3763cbe2408197d8e4680795c20b5d11
ksinghj <kartarsjabanda@gmail.com>
2023-01-22 18:14:15 +0000

    feat(home): adds c2a button and logo

---
+++ src/components/C2AButton.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+export default function C2AButton() {
+  return (
+    <a
+      href='https://flashapp.cards'
+      aria-describedby='Link to Flash app'
+      className='p-2 font-semibold px-4 text-black bg-flash-yellow rounded-full'>
+      Go to app
+    </a>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 // import styles from '@/styles/Home.module.css'
 
+import flashLogoText from '../assets/logo-text.png'
+import C2AButton from '@/components/C2AButton'
+
 export default function Home() {
   return (
     <>
@@ -11,7 +14,12 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <div>
-        <h1 className='text-white'>Flash</h1>
+        <header className='flex flex-row justify-between items-center mb-8 sticky top-0 left-0 w-full'>
+          <Image src={flashLogoText} alt='Flash logo' height={40} />
+          <C2AButton />
+        </header>
+        <h1 className='text-white text-4xl text-center font-semibold mb-6'>Flash</h1>
+        <h2 className='text-white text-xl text-center font-semibold'>Flashcards made easy.</h2>
       </div>
     </>
   )
