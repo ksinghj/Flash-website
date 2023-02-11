4d9c255ad989ac10a82bb2a32f47201dc803f2dd
ksinghj <kartarsjabanda@gmail.com>
2023-02-11 14:06:26 +0100

    feat(redesign): moves sections to design

--- src/components/C2AButton.tsx
+++ src/components/C2AButton.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
 
-export default function C2AButton({ className }: { className?: string }) {
+export default function C2AButton({ className, children }: { className?: string; children: React.ReactNode }) {
   return (
     <a
       href='https://flashapp.cards'
       aria-describedby='Link to Flash app'
       className={`p-2 font-semibold px-4 text-center text-black bg-flash-yellow rounded-full ${className}`}>
-      Go to app
+      {children}
     </a>
   )
 }
---
+++ src/components/InteractiveDeck.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function InteractiveDeck() {
+  return <p>Interactive Deck</p>
+}
